7c3754ba5727f147d3b5e283ef9c26f9a39824c5
a109593 <a109593@alunos.uminho.pt>
2026-05-08 00:42:16 +0100

    fase3 menos opcional

--- src/controller.c
+++ src/controller.c
@@ -102,6 +102,7 @@ int main (int argc, char *argv[]) {
                     }
                 }
                 if(justo){
+                    req_arr[i].status = 1;
                     running++;
                     lancados++;
                     execucoes_por_user[req_arr[i].user_id]++;
